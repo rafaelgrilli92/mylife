033ec25464032d308d7c8186cf809daf35f4d47e
Rafael Grilli <rafaelgrilli92@hotmail.com>
2017-06-13 00:29:51 +1000

    adding vs code debugger config

--- helpers/auth.js
+++ helpers/auth.js
@@ -8,27 +8,30 @@ const User = require('../models/user');
 function requireSignIn(req, res, next) {
     passport.authenticate('local', { session: false }, (err, email, password) => {
         // Try to find the user on the database
-        User.findOne({ email: email }, (err, user) => {
-            if (err) return done(err);
-
+        User.findOne({ email: email })
+        .then(user => {
             // If the user doesn't exist
-            if (!user) return httpResponse.wrong(res, statusCode.error.CONFLICT, `The email ${email} doesn't exist`);
+            if (!user) return httpResponse.wrong(res, statusCode.error.CONFLICT, `The email doesn't exist`);
 
             // Compare passwords
             user.comparePassword(password, (err, isMatch) => {
-                if (err) return done(err);
+                if (err) return httpResponse.error(res, err, "Error while comparing user's password");
 
                 // If it isn't correct
-                if (!isMatch) return done(null, false);
+                if (!isMatch) return httpResponse.wrong(res, statusCode.error.CONFLICT, `Invalid password`);
                 
                  // If it's correct
                 req.login(user, loginErr => {
                     if (loginErr) {
                         return next(loginErr);
                     }
+
                     return res.send({ success : true, message : 'authentication succeeded' });
                 });      
             })
+        })
+        .catch(err => {
+            return httpResponse.error(res, err, 'Error while finding the user on database');
         })
     })(req, res, next);
 }
--- services/passport.js
+++ services/passport.js
@@ -6,26 +6,38 @@ const User = require('../models/user');
  * LOCAL STRATEGY
  */
 const localLoginOptions = { usernameField: 'email' };
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user){
+        if(!err) done(null, user);
+        else done(err, null)  
+    })
+});
 const localLogin = new LocalStrategy(localLoginOptions, function(email, password, done) {
     return done(null, email, password);
-    // // Try to find the user on the database
-    // User.findOne({ email: email }, function(err, user) {
-    //     if (err) return done(err);
+    // Try to find the user on the database
+    User.findOne({ email: email })
+    .then(user => {
+        // If the user doesn't exist
+        if (!user) return done(null, false);
 
-    //     // If the user doesn't exist
-    //     if (!user) return done(null, false);
+        // Compare passwords
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) return done(err);
 
-    //     // Compare passwords
-    //     user.comparePassword(password, function(err, isMatch) {
-    //         if (err) return done(err);
-
-    //         // If it isn't correct
-    //         if (!isMatch) return done(null, false);
+            // If it isn't correct
+            if (!isMatch) return done(null, false);
             
-    //          // If it's correct
-    //         return done(null, user);
-    //     })
-    // })
+             // If it's correct
+            return done(null, user);
+        })
+    })
+    .catch(err => {
+        return done(err);
+    })
 });
 
 passport.use(localLogin);
